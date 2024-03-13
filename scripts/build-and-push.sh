bdd78ac8f1dc930407fd69350bc3bae713fb94a3
Josh Faigan <djjoker13@gmail.com>
2024-03-13 16:38:58 -0400

    flipping back to github actions

--- scripts/build-and-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-docker-compose -p dev-build build app
-
-docker tag "dev-build_app" "evilgenius13/azerothcontainers:latest"
-
-docker push "evilgenius13/azerothcontainers:latest"
--- scripts/notify-discord-failure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WEBHOOK_URL=$DISCORD_WEBHOOK_URL
-
-curl -H "Content-Type: application/json" \
-     -X POST \
-     -d '{"content": "Build failed: :cross_mark:"}' \
-     $WEBHOOK_URL
