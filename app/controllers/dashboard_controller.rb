c846e32363727c8ed92ee863180a3ec512b62baa
Josh Faigan <djjoker13@gmail.com>
2024-03-16 15:25:36 -0400

    added stats, and reg/debug logging to containers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @info = Docker.info
+  rescue Docker::Error::DockerError => e
+    @error = "Failed to connect to Docker daemon #{e.message}"
   end
 end
