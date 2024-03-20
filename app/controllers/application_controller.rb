6bfbbf076d9e7a4e778dd50e427666bec963afee
Josh Faigan <djjoker13@gmail.com>
2024-03-20 13:19:54 -0400

    rework of most route views and unsquished them from sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_docker_info
+  private
+
+  def set_docker_info
+    @info = Docker.info
+  rescue Docker::Error::DockerError => e
+    @error = "Failed to connect to Docker daemon #{e.message}"
+  end
 end
